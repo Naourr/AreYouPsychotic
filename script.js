59ce92378d0728892531bab0ee028eddbc061c1a
Naourr <princessmarielledepalubos2nd@gmail.com>
2025-06-19 23:02:02 +0800

    add slide in from right but incomplete

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
 const menubar = document.querySelector('.menubar');
 const menu = document.querySelector('.menu');
+const usli1 = document.querySelector('.usli1');
+const usli2 = document.querySelector('.usli2');
+const usli1bar = document.querySelector('.usli1bar');
+const usli2bar = document.querySelector('.usli2bar');
+
 
 
 menu.addEventListener('click', function(){
 	menubar.classList.toggle('active');
 });
-
-
-
-// Trigger CSS Animations when elements are scrolled into view
-
-// This JS uses the Intersection Observer API to determine if objects are within the viewport
-// It addes an 'in-view' class to elements when they come into view (and removes the class when not on screen)
-// Use to add @keyframe or transition animations to elements so they animate once they are on screen
-
-//TO USE
-// Simply add the .animate class to those HTML elements that you wish to animate. For example, <h1 class="animate">
-// Once in the viewport, the JS will add the 'in-view' class to those elements. For example, <h1 class="animate in-view">
-// Define your CSS to enable animations once that element is in view. For example, h1.in-view { }
-
-//Check if the document is loaded (so that this script can be placed in the <head>)
+usli1.addEventListener('click', function(){
+	usli1bar.classList.toggle('active');
+});
+usli2.addEventListener('click', function(){
+	usli2bar.classList.toggle('active');
+});
 document.addEventListener("DOMContentLoaded", () => {
-
-	// Use Intersection Observer to determine if objects are within the viewport
 	const observer = new IntersectionObserver(entries => {
 	  entries.forEach(entry => {
 		if (entry.isIntersecting) {
@@ -32,11 +26,6 @@ document.addEventListener("DOMContentLoaded", () => {
 		entry.target.classList.remove('in-view');
 	  });
 	});
-
-	// Get all the elements with the .animate class applied
 	const allAnimatedElements = document.querySelectorAll('.animate');
-
-	// Add the observer to each of those elements
 	allAnimatedElements.forEach((element) => observer.observe(element));
-
 }); 
